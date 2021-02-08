44f0127a21cde41288a73d40621f7dc9b40e691e
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2021-02-08 20:33:49 +0100

    Solved key error Michael Ruttner

--- install.R
+++ install.R
@@ -1,4 +1,9 @@
-script <- paste('sudo apt-get update
+# Michael Ruttner key error: 
+# solved here https://www.bentoh.my/post/gpg-key-problem/
+script <- paste('
+sudo apt-key del "E298 A3A8 25C0 D65D FD57 CBB6 5171 6619 E084 DAB9"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update

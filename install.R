74b7ae507a94c6b6153759c138532c8240193349
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2021-02-08 20:45:56 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,8 @@
 # Michael Ruttner key error: 
 # solved here https://www.bentoh.my/post/gpg-key-problem/
+
+# In case of dpkg errors run https://askubuntu.com/questions/219545/dpkg-error-dpkg-status-database-is-locked-by-another-process
+
 script <- paste('
 sudo apt-key del "E298 A3A8 25C0 D65D FD57 CBB6 5171 6619 E084 DAB9"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9

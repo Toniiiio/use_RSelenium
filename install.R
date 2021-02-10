cf93a49676c8ffac86bf2daa06d3b18a2e443f7a
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2021-02-10 23:17:01 +0100

    Update install.R

--- install.R
+++ install.R
@@ -7,10 +7,19 @@ script <- paste('
 sudo apt-key del "E298 A3A8 25C0 D65D FD57 CBB6 5171 6619 E084 DAB9"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo apt-get update
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+# this works (second) not sure about the first one 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+##sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get install -y docker-engine
+
+
+# sudo apt-get install -y docker-engine
+# use instead
+https://stackoverflow.com/a/39645216/3502164
+
 sudo service docker start
 sudo docker run hello-world
 sudo docker run -d -p 4445:4444 selenium/standalone-firefox
